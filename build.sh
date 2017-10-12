fd21dd4168bbd76b4d3d58123447d8676799c2a7
Andrew Noyes <anoyes34@gmail.com>
2017-10-11 21:41:43 -0700

    Download ninja release from github

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@
 set -euxo pipefail
 
 mkdir -p build
+if ! [ -f build/ninja ] ; then
+    wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
+    unzip ninja-linux.zip
+    mv ninja build
+    rm ninja-linux.zip
+fi
 meson -Db_coverage=true build
 cd build
-ninja
+./ninja
